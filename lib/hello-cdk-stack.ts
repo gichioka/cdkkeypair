0b017f1b812cbd97e26c92cbe7dc6ae9298fee2d
root <root@ip-172-31-2-239.ap-northeast-1.compute.internal>
2022-06-29 10:40:41 +0000

    first commit

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,18 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-  
-    new Vpc(this, 'Vpc');
+ 
+     const vpc = new ec2.Vpc(this, 'TheVPC', {
+      cidr: "10.0.0.0/16"
+    })
+
+    const i = new ec2.Instance(this, id, {
+      instanceType: new ec2.InstanceType("t2.medium"),
+      machineImage: new ec2.AmazonLinuxImage,
+      vpc: vpc,
+    })
   }
 }
-// The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
