a83b291cb535bb8fb0974d7b09383db9de77589d
root <root@ip-172-31-3-151.ap-northeast-1.compute.internal>
2022-07-03 10:42:22 +0000

    first commit

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,22 +1,46 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { Construct } from 'constructs';
+import "source-map-support/register"
+import {Construct} from 'constructs'
+import {App, CfnOutput, RemovalPolicy, Stack, StackProps, Token} from 'aws-cdk-lib'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
- 
-     const vpc = new ec2.Vpc(this, 'TheVPC', {
-      cidr: "10.0.0.0/16"
+
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [{
+        name: 'PublicSubnet',
+        subnetType: ec2.SubnetType.PUBLIC,
+      }],
     })
 
-    const i = new ec2.Instance(this, id, {
-      instanceType: new ec2.InstanceType("t2.medium"),
-      machineImage: new ec2.AmazonLinuxImage,
-      vpc: vpc,
+    // キーペア作成
+    const cfnKeyPair = new ec2.CfnKeyPair(this, 'CfnKeyPair', {
+      keyName: 'test-key-pair',
     })
+    cfnKeyPair.applyRemovalPolicy(RemovalPolicy.DESTROY)
+
+    // キーペア取得コマンドアウトプット
+    new CfnOutput(this, 'GetSSHKeyCommand', {
+      value: `aws ssm get-parameter --name /ec2/keypair/${cfnKeyPair.getAtt('KeyPairId')} --region ${this.region} --with-decryption --query Parameter.Value --output text`,
+    })
+
+    // EC2作成
+    const instance = new ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.T3,
+          ec2.InstanceSize.NANO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName: Token.asString(cfnKeyPair.ref),
+    })
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(22))
   }
 }
-
-
+ 
